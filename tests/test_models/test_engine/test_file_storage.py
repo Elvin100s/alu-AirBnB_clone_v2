1e8dd2c976202c9754fedd4560605871efd88b33
Elvin <e.cyubahiro@alustudent.com>
2025-05-21 12:54:57 +0200

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 import os
+import json
 
 
 class test_fileStorage(unittest.TestCase):
@@ -11,17 +12,17 @@ class test_fileStorage(unittest.TestCase):
 
     def setUp(self):
         """ Set up test environment """
-        del_list = []
-        for key in storage._FileStorage__objects.keys():
-            del_list.append(key)
-        for key in del_list:
-            del storage._FileStorage__objects[key]
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+        storage._FileStorage__objects = {}
 
     def tearDown(self):
         """ Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except:
+        except FileNotFoundError:
             pass
 
     def test_obj_list_empty(self):
@@ -31,15 +32,17 @@ class test_fileStorage(unittest.TestCase):
     def test_new(self):
         """ New object is correctly added to __objects """
         new = BaseModel()
-        for obj in storage.all().values():
-            temp = obj
-        self.assertTrue(temp is obj)
+        storage.new(new)
+        obj_dict = storage.all()
+        self.assertIn(f'BaseModel.{new.id}', obj_dict)
+        self.assertEqual(obj_dict[f'BaseModel.{new.id}'], new)
 
     def test_all(self):
         """ __objects is properly returned """
         new = BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
+        self.assertIn(f'BaseModel.{new.id}', temp)
 
     def test_base_model_instantiation(self):
         """ File is not created on BaseModel save """
@@ -59,12 +62,16 @@ class test_fileStorage(unittest.TestCase):
         new = BaseModel()
         storage.save()
         self.assertTrue(os.path.exists('file.json'))
+        with open('file.json', 'r') as f:
+            data = json.load(f)
+        self.assertIn(f'BaseModel.{new.id}', data)
 
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
         new = BaseModel()
         storage.save()
         storage.reload()
+        loaded = None
         for obj in storage.all().values():
             loaded = obj
         self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
@@ -72,12 +79,14 @@ class test_fileStorage(unittest.TestCase):
     def test_reload_empty(self):
         """ Load from an empty file """
         with open('file.json', 'w') as f:
-            pass
-        with self.assertRaises(ValueError):
-            storage.reload()
+            f.write("{}")
+        storage.reload()
+        self.assertEqual(len(storage.all()), 0)
 
     def test_reload_from_nonexistent(self):
         """ Nothing happens if file does not exist """
+        if os.path.exists('file.json'):
+            os.remove('file.json')
         self.assertEqual(storage.reload(), None)
 
     def test_base_model_save(self):
@@ -97,13 +106,28 @@ class test_fileStorage(unittest.TestCase):
     def test_key_format(self):
         """ Key is properly formatted """
         new = BaseModel()
-        _id = new.to_dict()['id']
-        for key in storage.all().keys():
-            temp = key
-        self.assertEqual(temp, 'BaseModel' + '.' + _id)
+        storage.new(new)
+        key = f'BaseModel.{new.id}'
+        self.assertIn(key, storage.all().keys())
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
         from models.engine.file_storage import FileStorage
-        print(type(storage))
         self.assertEqual(type(storage), FileStorage)
+
+    def test_delete(self):
+        """ Test delete method """
+        new = BaseModel()
+        storage.new(new)
+        storage.save()
+        storage.delete(new)
+        self.assertNotIn(new, storage.all().values())
+
+    def test_multiple_objects(self):
+        """ Test storage with multiple objects """
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        obj3 = BaseModel()
+        storage.save()
+        storage.reload()
+        self.assertEqual(len(storage.all()), 3)
