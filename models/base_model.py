36cf8d1e3a9ff36634f9e72eb9800b1a0ac87c65
Elvin <e.cyubahiro@alustudent.com>
2025-05-21 13:33:59 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""Defines the BaseModel class that all other classes will inherit from."""
+import uuid
 from datetime import datetime
+import models
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+# Set up SQLAlchemy Base if using DB storage
 if models.storage_type == 'db':
     Base = declarative_base()
 else:
     Base = object
 
 class BaseModel:
-    """Defines the BaseModel class."""
+    """The BaseModel class from which all other classes will inherit"""
     
     if models.storage_type == 'db':
         id = Column(String(60), primary_key=True, nullable=False)
@@ -20,37 +21,45 @@ class BaseModel:
         updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel."""
-        self.id = str(uuid4())
+        """Initialization of the base model"""
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
+                    # Handle both string and datetime inputs
+                    if isinstance(value, str):
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+                elif key != '__class__':
                     setattr(self, key, value)
 
+    def __str__(self):
+        """String representation of the BaseModel instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
-        """Update updated_at with current time and save to storage."""
+        """Updates updated_at with current time and saves to storage"""
         self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Return a dictionary representation of the BaseModel instance."""
-        my_dict = self.__dict__.copy()
-        my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        my_dict.pop('_sa_instance_state', None)
-        return my_dict
+        """Returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        
+        # Remove SQLAlchemy-specific attribute if it exists
+        if '_sa_instance_state' in new_dict:
+            del new_dict['_sa_instance_state']
+            
+        return new_dict
 
     def delete(self):
-        """Delete the current instance from storage."""
+        """Deletes the current instance from the storage"""
         models.storage.delete(self)
-
-    def __str__(self):
-        """Return the string representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
