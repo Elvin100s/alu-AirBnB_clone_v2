22300678f0a4ee8c2e802f6b076eecf72d5a3c1d
Elvin <e.cyubahiro@alustudent.com>
2025-05-25 23:38:58 +0200

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""Tests for DBStorage"""
-
 import unittest
 import models
 from models.user import User
@@ -12,64 +10,25 @@ from models.city import City
 import os
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "skip if not DB storage")
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "skip if not db")
 class TestDBStorage(unittest.TestCase):
-    """Tests for DBStorage"""
+    """DB Storage test"""
 
     def setUp(self):
+        """ Set up test environment """
         self.storage = models.storage
 
     def tearDown(self):
+        """ Remove storage file at end of tests """
         del self.storage
 
     def test_user(self):
-        user = User(name="Chyna", email="chyna@gmail.com", password="Chyna12345")
+        """ Tests user """
+        user = User(
+            name="Chyna",
+            email="chyna@gmail.com",
+            password="Chyna12345"
+        )
         user.save()
-        self.assertIn(f"User.{user.id}", self.storage.all().keys())
-        self.assertEqual(user.name, "Chyna")
-
-    def test_city(self):
-        state = State(name="California")
-        state.save()
-        city = City(name="Batch", state_id=state.id)
-        city.save()
-        self.assertIn(f"City.{city.id}", self.storage.all().keys())
-        self.assertEqual(city.name, "Batch")
-
-    def test_state(self):
-        state = State(name="California")
-        state.save()
-        self.assertIn(f"State.{state.id}", self.storage.all().keys())
-        self.assertEqual(state.name, "California")
-
-    def test_place(self):
-        state = State(name="California")
-        state.save()
-        city = City(name="Batch", state_id=state.id)
-        city.save()
-        user = User(name="Chyna", email="chyna@gmail.com", password="Chyna12345")
-        user.save()
-        place = Place(name="Palace", number_rooms=4, city_id=city.id, user_id=user.id)
-        place.save()
-        self.assertIn(f"Place.{place.id}", self.storage.all().keys())
-        self.assertEqual(place.name, "Palace")
-
-    def test_amenity(self):
-        amenity = Amenity(name="Starlink")
-        amenity.save()
-        self.assertIn(f"Amenity.{amenity.id}", self.storage.all().keys())
-        self.assertEqual(amenity.name, "Starlink")
-
-    def test_review(self):
-        state = State(name="California")
-        state.save()
-        city = City(name="Batch", state_id=state.id)
-        city.save()
-        user = User(name="Chyna", email="chyna@gmail.com", password="Chyna12345")
-        user.save()
-        place = Place(name="Palace", number_rooms=4, city_id=city.id, user_id=user.id)
-        place.save()
-        review = Review(text="no comment", place_id=place.id, user_id=user.id)
-        review.save()
-        self.assertIn(f"Review.{review.id}", self.storage.all().keys())
-        self.assertEqual(review.text, "no comment")
+        self.assertIn("User.{}".format(user.id), self.storage.all().keys())
+        self.assertEqual(us
