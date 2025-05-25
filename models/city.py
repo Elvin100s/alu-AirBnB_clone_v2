70bf840cdc397d5c1229f55c8da6f4306d6f1f67
Elvin <e.cyubahiro@alustudent.com>
2025-05-25 04:33:57 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -3,33 +3,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import models
+import os
 
 
 class City(BaseModel, Base):
     """Representation of City"""
-    if models.storage_type == "db":
-        __tablename__ = 'cities'
-        
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        
-        # Relationship with Place - cascade delete when City is deleted
-        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
-    else:
+    __tablename__ = 'cities'
+    
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    
+    places = relationship("Place", backref="cities", cascade="all, delete-orphan")
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         state_id = ""
         name = ""
-
-    @property
-    def places(self):
-        """Getter attribute that returns the list of Place instances with
-        city_id equals to the current City.id
-        FileStorage relationship between City and Place
-        """
-        if models.storage_type != "db":
-            place_list = []
-            all_places = models.storage.all("Place")
-            for place in all_places.values():
-                if place.city_id == self.id:
-                    place_list.append(place)
-            return place_list
