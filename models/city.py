c2755cb878feb6f5e7edac795f531a53a4d59ac6
Elvin <e.cyubahiro@alustudent.com>
2025-05-25 22:42:37 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -10,19 +10,19 @@ class City(BaseModel, Base):
     """Representation of City"""
     __tablename__ = 'cities'
 
+    
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-<<<<<<< HEAD
 
     places = relationship(
         "Place",
         backref="cities",
-        cascade="all, delete-orphan")
-=======
->>>>>>> a1a7597a2c04a4f2c17ec7dabcbd2bca01cee907
+        cascade="all, delete-orphan"
+    )
 
-    places = relationship("Place", backref="cities",
-                         cascade="all, delete-orphan")
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         state_id = ""
         name = ""
+
+
+# Two blank lines after class definition
