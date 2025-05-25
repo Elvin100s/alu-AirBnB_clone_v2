d245ad10d5dd8eec40fd58eec07354564e8c9777
Elvin <e.cyubahiro@alustudent.com>
2025-05-25 22:47:35 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """User class"""
     __tablename__ = 'users'
-    
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
