bf7acbcdcdad98cd9c0aba0f0e72d1b768360340
Elvin <e.cyubahiro@alustudent.com>
2025-05-25 23:05:08 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the class TestConsoleDocs
-"""
+"""Tests for the console"""
 
 import console
 import inspect
@@ -11,7 +9,7 @@ HBNBCommand = console.HBNBCommand
 
 
 class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
+    """Tests for documentation and PEP8 of the console"""
 
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
@@ -31,12 +29,10 @@ class TestConsoleDocs(unittest.TestCase):
         """Test for the console.py module docstring"""
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1)
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1)
