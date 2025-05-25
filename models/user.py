b220bb4b7775bb8b12cb22bfee30bf47477f9ffa
Elvin <e.cyubahiro@alustudent.com>
2025-05-25 03:40:36 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""User class with complete functionality"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+import models
+
 
 class User(BaseModel, Base):
-    """User class with dual storage support"""
-    __tablename__ = 'users'
-    
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    """Representation of a user"""
+    if models.storage_type == "db":
+        __tablename__ = 'users'
+        
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        
+        # Relationship with Place - cascade delete when User is deleted
         places = relationship("Place", backref="user", cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+        # Relationship with reviews (if you have a Review model)
+        # reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize User with storage-appropriate attributes"""
-        super().__init__(*args, **kwargs)
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
-            self.email = kwargs.get('email', "")
-            self.password = kwargs.get('password', "")
-            self.first_name = kwargs.get('first_name', "")
-            self.last_name = kwargs.get('last_name', "")
