f79dd20fd825ac9dac5baf08b6e5195da825610d
Elvin <e.cyubahiro@alustudent.com>
2025-06-01 23:19:35 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,59 +1,57 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script that distributes an archive to web servers
 """
 
-from fabric.api import put, run, env
+from fabric.api import env, put, run, local
 from os.path import exists
-#!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+from datetime import datetime
 
-from fabric.api import put, run, env
-from os.path import exists
 env.hosts = ['34.227.11.202', '52.90.153.53']
-
-
-def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
-
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to web servers
+    
+    Args:
+        archive_path (str): Path to the archive to deploy
+        
+    Returns:
+        bool: True if successful, False otherwise
+    """
+    if not exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        # Upload archive
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+
+        # Create target directory
+        file_name = archive_path.split('/')[-1]
+        folder_name = file_name.split('.')[0]
+        release_path = '/data/web_static/releases/{}/'.format(folder_name)
+        run('mkdir -p {}'.format(release_path))
+
+        # Uncompress archive
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, release_path))
+
+        # Move files and clean up
+        run('mv {}/web_static/* {}'.format(release_path, release_path))
+        run('rm -rf {}/web_static'.format(release_path))
+        run('rm /tmp/{}'.format(file_name))
+
+        # Update symlink (critical fix)
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {} /data/web_static/current'.format(release_path))
+
+        # Verify deployment
+        if run('test -f /data/web_static/current/0-index.html').failed:
+            return False
+        if run('test -f /data/web_static/current/my_index.html').failed:
+            return False
+
         return True
-    except:
+    except Exception as e:
+        print("Error:", e)
         return False
